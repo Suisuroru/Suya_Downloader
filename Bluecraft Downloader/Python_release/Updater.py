cc2937432ee90fd12ef029783d99b147e89e4828
Suisuroru <qwertyuiop14077@qq.com>
2024-07-04 14:30:28 +0800

    update Updater.py 优化结构

--- Bluecraft Downloader/Python_release/Updater.py
+++ Bluecraft Downloader/Python_release/Updater.py
@@ -34,7 +34,8 @@ def show_message(partner, partner_en):
     root = tkinter.Tk()
     root.withdraw()  # 隐藏主窗口
     messagebox.showinfo("提示 / Tip",
-                        "更新已开始，在更新完成后，Suya Downloader将会自动启动，请等待自动重启，本次更新类型为{}。\nThe update has been started, after the update is finished, Suya Downloader will open automatically, please wait for the automatic reboot, the type of this update is {}".format(
+                        "更新已开始，在更新完成后，Suya Downloader将会自动启动，请等待自动重启，本次更新类型为{}。\n" + 
+                        "The update has been started, after the update is finished, Suya Downloader will open automatically, please wait for the automatic reboot, the type of this update is {}".format(
                             partner,partner_en))
 
 
