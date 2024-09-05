2b9cad15b26c3fa9269b06c1e0c3d8ab0c1ecd8c
Suisuroru <qwertyuiop14077@qq.com>
2024-09-05 22:37:09 +0800

    预更新Suya Downloader主程序版本至1.0.3.3

--- Launcher.py
+++ Launcher.py
@@ -20,7 +20,7 @@ import pygame
 import requests
 from PIL import Image, ImageTk
 
-Suya_Downloader_Version = "1.0.3.2"
+Suya_Downloader_Version = "1.0.3.3"
 
 # 获取运行目录
 current_working_dir = os.getcwd()
